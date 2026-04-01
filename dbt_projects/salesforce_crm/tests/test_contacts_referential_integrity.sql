d2d53f42119f8697fde193dbef7e5ed33ed3b391
sumesh1812 <85581109+sumesh1812@users.noreply.github.com>
2026-04-01 15:51:08 -0700

    dbt: add tests/test_contacts_referential_integrity.sql

---
+++ dbt_projects/salesforce_crm/tests/test_contacts_referential_integrity.sql
@@ -0,0 +1,8 @@
+select
+    c.contact_id,
+    c.account_id
+from {{ ref('stg_contacts') }} c
+left join {{ ref('stg_accounts') }} a
+    on c.account_id = a.account_id
+where c.account_id is not null
+    and a.account_id is null
